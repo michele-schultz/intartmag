ce527631a16c8f73f0c35a2937a899bbf0db3692
adamaslan <chillcoders@gmal.com>
2025-12-29 19:00:02 -0500

    nav2

--- navbar-component.js
+++ navbar-component.js
@@ -1,42 +1,51 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <nav class="navbar navbar-expand-md bg-fade fixed-top">
+    <a class="navbar-brand" href="/">I. A. M.</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarsExampleDefault"
+      aria-controls="navbarsExampleDefault"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/pages/galleries.html">Galleries</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/pages/culture.html">Culture</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+`;
+
 class SiteNavbar extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `
-      <nav class="navbar navbar-expand-md bg-fade fixed-top">
-        <a class="navbar-brand" href="/">I. A. M.</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarsExampleDefault"
-          aria-controls="navbarsExampleDefault"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="/">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/pages/galleries.html">Galleries</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/pages/culture.html">Culture</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    `;
+    this.appendChild(template.content.cloneNode(true));
+    this.setActiveLink();
+  }
 
-    // Mark current page as active
+  setActiveLink() {
     const currentPath = window.location.pathname;
+
     this.querySelectorAll('.nav-link').forEach(link => {
       const href = link.getAttribute('href');
-      if (currentPath === href ||
-          (href === '/' && (currentPath === '/' || currentPath === '/index.html')) ||
-          currentPath.endsWith(href)) {
+      const isHome = href === '/';
+      const isActive = isHome
+        ? currentPath === '/' || currentPath === '/index.html'
+        : currentPath === href;
+
+      if (isActive) {
         link.closest('.nav-item').classList.add('active');
       }
     });
